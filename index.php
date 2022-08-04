0dd6486861cd2441ba0d86db11ba7de2aebdb6b6
Khushi Agarwal <khushiagarwal78910@gmail.com>
2022-08-04 13:12:29 +0530

    first page completed

--- index.php
+++ index.php
@@ -475,7 +475,7 @@
             </div>
         </div>
     </div>
-<!-- our facilities -->
+    <!-- our facilities -->
     <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Our Facilities</h2>
 
     <div class="container">
@@ -521,31 +521,180 @@
 
         </div>
     </div>
-<!-- testimonials -->
+    <!-- testimonials -->
 
-<h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Testimonials</h2>
-<div class="container">
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Testimonials</h2>
+    <div class="container mt-5">
 
-    <!-- Swiper -->
-    <div class="swiper swiper-testimonials">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide">
-      
-          </div>
-          
-         
+        <!-- Swiper -->
+        <div class="swiper swiper-testimonials">
+            <div class="swiper-wrapper mb-5">
+
+
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="Images/facilities/IMG_43553.svg" width="30px">
+                        <h6 class="m-0 ms-2">Random User1</h6>
+
+                    </div>
+
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quam corporis ducimus officia
+                        illo! Ab sequi vel voluptate accusantium animi? </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill  text-warning"></i>
+                    </div>
+                </div>
+
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="Images/facilities/IMG_43553.svg" width="30px">
+                        <h6 class="m-0 ms-2">Random User1</h6>
+
+                    </div>
+
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quam corporis ducimus officia
+                        illo! Ab sequi vel voluptate accusantium animi? </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill  text-warning"></i>
+                    </div>
+                </div>
+
+
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="Images/facilities/IMG_43553.svg" width="30px">
+                        <h6 class="m-0 ms-2">Random User1</h6>
+
+                    </div>
+
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quam corporis ducimus officia
+                        illo! Ab sequi vel voluptate accusantium animi? </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill  text-warning"></i>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+            <div class="swiper-pagination"></div>
+           
         </div>
-        <div class="swiper-pagination"></div>
-      </div>
-</div>
-    <br><br><br>
-    <br><br><br>
+
+
+
+        <div class="col-lg-12 text-center mt-5">
+            <a href="#" class="btn btn-sm btn-outline-dark rounded-0 shadow-none">know more >>></a>
+        </div>
+    </div>
+
+    <!-- Reach us -->
+
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Reach Us</h2>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+                <iframe class="w-100 rounded" height="320 px"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115330.43462063557!2d81.80158454999999!3d25.40226375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398534c9b20bd49f%3A0xa2237856ad4041a!2sPrayagraj%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1659596052250!5m2!1sen!2sin"
+                    height="450" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Call Us</h5>
+                    <a href="tel: +91999999999" class="d-inline-block mb-2 text-decoration-none text-dark"><i
+                            class="bi bi-telephone-fill"></i> +91999999999</a>
+                    <br>
+                    <a href="tel: +91999999999" class="d-inline-block mb-2 text-decoration-none text-dark"><i
+                            class="bi bi-telephone-fill"></i> +91999999999</a>
+
+                </div>
+
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Follow Us</h5>
+                    <a href="#" class="d-inline-block mb-3 "><span class="badge bg-light text-dark fs-6 p-2"><i
+                                class="bi bi-twitter me-1"></i> Twitter</span></a>
+                    <br>
+                    <a href="#" class="d-inline-block mb-3 "><span class="badge bg-light text-dark fs-6 p-2"><i
+                                class="bi bi-facebook me-1"></i> Facebook</span></a>
+                    <br>
+                    <a href="#" class="d-inline-block mb-3 "><span class="badge bg-light text-dark fs-6 p-2"><i
+                                class="bi bi-instagram me-1"></i> Instagram</span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid bg-white mt-5">
+        <div class="row">
+            <div class="col-lg-4 p-4">
+                <h3 class="h-font fw-bold fs-3 mb-2">TJ Hotel</h3>
+                <p>
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod enim molestiae cupiditate tempora
+                    voluptatum sed iste laudantium magni reprehenderit odit.
+                </p>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Links</h5>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a>
+                <br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a>
+                <br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a>
+                <br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a>
+                <br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 cloass="mb-3">Follow Us</h5>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none ">
+                    <i class="bi bi-twitter me-1"></i> Twitter</a>
+                <br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none ">
+                    <i class="bi bi-instagram me-1"></i> Instagram</a>
+                <br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none ">
+                    <i class="bi bi-facebook me-1"></i> Facebook</a>
+                <br>
+            </div>
+        </div>
+    </div>
+
+
+
+    <h6 class="text-center bg-dark text-white p-3 m-0">Designed and developed by TJ Webdev</h6>
+
+
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
         </script>
+
+
+
+
+
 
     <!-- Initialize Swiper -->
     <script>
@@ -560,25 +709,41 @@
         });
     </script>
 
-     <!-- Initialize Swiper for second -->
-     <script>
+    <!-- Initialize Swiper for second -->
+    <script>
         var swiper = new Swiper(".swiper-testimonials", {
-          effect: "coverflow",
-          grabCursor: true,
-          centeredSlides: true,
-          slidesPerView: "auto",
-          coverflowEffect: {
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          },
-          pagination: {
-            el: ".swiper-pagination",
-          },
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            slidesPerView: "3",
+            loop: true,
+            coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                640: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    sliderPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+            }
         });
-      </script>
+    </script>
 </body>
 
 </html>
